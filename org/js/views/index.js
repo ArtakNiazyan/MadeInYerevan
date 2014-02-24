c318f6a42a7d718b5201bd137db80b4dde74c116
vachi <vache@dendritecorp.com>
2014-02-24 16:36:27 +0400

    dd

--- org/js/views/index.js
+++ org/js/views/index.js
@@ -94,7 +94,7 @@
                                 $("ul.startups").append(
                                     that.startupSingle(m.toJSON())
                                 );
-                            }                            
+                            });                            
                         }
                         
                     }, 
